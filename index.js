778cfe67c8e054e665290a0268f142f35650b51d
Rami-Stalker <m.rami.stalker@gmail.com>
2025-10-23 09:50:50 +0300

    Fixed the construction and .gitingnore

--- index.js
+++ index.js
@@ -1,62 +1,3 @@
-// // IMPORTS FROM PACKAGES
-// require('dotenv').config();
-// const express = require("express");
-// const http = require('http');
-// const { Server } = require("socket.io");
-
-// // IMPORTS FROM OTHER FILES
-// const connection = require("./Utils/connection");
-// const socketIOEvents = require("./middlewares/socket");
-// const authMiddleware = require("./middlewares/auth");
-
-// const authRouter = require("./routes/auth");
-// const postRouter = require("./routes/post");
-// const storyRouter = require("./routes/story");
-// const userRouter = require("./routes/user");
-// const chatRouter = require("./routes/chat");
-// const notificationRouter = require("./routes/notification");
-
-// // INIT
-// let port = null;
-// let app = null;
-// let server = null;
-// let io = null;
-
-// const initVar = async () => {
-//     port = process.env.PORT;
-//     app = express();
-//     server = http.createServer(app);
-//     io = new Server(server);
-
-//     await connection();
-// }
-
-// // middleware
-// const middleware = () => {
-//     app.use(express.urlencoded({ extended: false }));
-//     app.use(express.json({ extended: false }));
-
-//     app.use("/api/authentication", authRouter);
-//     app.use("/api/user", userRouter);
-//     app.use("/api/post", postRouter);
-//     app.use("/api/story", storyRouter);
-//     app.use("/api/chat", chatRouter);
-//     app.use("/api/notification", notificationRouter);
-// }
-
-// const ListenToPort = () => {
-//     server.listen(port, () => {
-//         console.log("Rest Api Port Connected " + port);
-//     });
-// }
-
-// // Connections
-// initVar().then(() => {
-//     middleware();
-//     ListenToPort();
-//     socketIOEvents(io);
-// });
-
 require('dotenv').config();
 const express = require("express");
 const http = require('http');
@@ -84,11 +25,7 @@ const io = new Server(server);
 const initApp = async () => {
     try {
         await connection();
-        // app.use(authMiddleware);
-        // const userIdToTrack = app.locals.userId;
-        // createChangeStream(userIdToTrack);
         createCshangeStream("652ac4045f43862232aa930b");
-        // initMeetingServer(server);
     } catch (error) {
         console.error("Error during initialization:", error);
         process.exit(1);
